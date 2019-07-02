ada6a1a5b947e16857889a81aea077d6a1bd7c72
Anton Nikolov <AntonYurievNikolov@gmail.com>
2019-07-02 12:54:32 +0300

    CNN with image Augmentations to enlarge the Dataset

--- R/convolutional image recognition.R
+++ R/convolutional image recognition.R
@@ -143,3 +143,97 @@ history <- model %>% fit_generator(
 #Save the model
 model %>% save_model_hdf5("cats_and_dogs_small_1.h5")
 plot(history)
+
+#Image generation with Augmentation
+datagen <- image_data_generator(
+  rescale = 1/255,
+  rotation_range = 40,
+  width_shift_range = 0.2,
+  height_shift_range = 0.2,
+  shear_range = 0.2,
+  zoom_range = 0.2,
+  horizontal_flip = TRUE,
+  fill_mode = "nearest"
+)
+# Test Augmentations ####
+# # We pick one image to "augment"
+# fnames <- list.files(train_cats_dir, full.names = TRUE)
+# img_path <- fnames[[28]] 
+# # Convert it to an array with shape (150, 150, 3)
+# img <- image_load(img_path, target_size = c(150, 150))
+# img_array <- image_to_array(img)
+# img_array <- array_reshape(img_array, c(1, 150, 150, 3))
+# # Generated that will flow augmented images
+# augmentation_generator <- flow_images_from_data(
+#   img_array, 
+#   generator = datagen, 
+#   batch_size = 1 
+# )
+# # Plot the first 4 augmented images
+# op <- par(mfrow = c(2, 2), pty = "s", mar = c(1, 0, 1, 0))
+# for (i in 1:4) {
+#   batch <- generator_next(augmentation_generator)
+#   plot(as.raster(batch[1,,,]))
+# }
+# par(op)
+
+#New Network with dropout####
+model <- keras_model_sequential() %>% 
+  layer_conv_2d(filters = 32, kernel_size = c(3, 3), activation = "relu",
+                input_shape = c(150, 150, 3)) %>% 
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>% 
+  layer_conv_2d(filters = 64, kernel_size = c(3, 3), activation = "relu") %>% 
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>% 
+  layer_conv_2d(filters = 128, kernel_size = c(3, 3), activation = "relu") %>% 
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>% 
+  layer_conv_2d(filters = 128, kernel_size = c(3, 3), activation = "relu") %>% 
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>% 
+  layer_flatten() %>% 
+  layer_dropout(rate = 0.5) %>% 
+  layer_dense(units = 512, activation = "relu") %>% 
+  layer_dense(units = 1, activation = "sigmoid")  
+
+model %>% compile(
+  loss = "binary_crossentropy",
+  optimizer = optimizer_rmsprop(lr = 1e-4),
+  metrics = c("acc")
+)
+
+datagen <- image_data_generator(
+  rescale = 1/255,
+  rotation_range = 40,
+  width_shift_range = 0.2,
+  height_shift_range = 0.2,
+  shear_range = 0.2,
+  zoom_range = 0.2,
+  horizontal_flip = TRUE
+)
+
+test_datagen <- image_data_generator(rescale = 1/255)
+
+train_generator <- flow_images_from_directory(
+  train_dir,
+  datagen,
+  target_size = c(150, 150),
+  batch_size = 32,
+  class_mode = "binary"
+)
+
+validation_generator <- flow_images_from_directory(
+  validation_dir,
+  test_datagen,
+  target_size = c(150, 150),
+  batch_size = 32,
+  class_mode = "binary"
+)
+
+history <- model %>% fit_generator(
+  train_generator,
+  steps_per_epoch = 100,
+  epochs = 10,#Should be 100
+  validation_data = validation_generator,
+  validation_steps = 50
+)
+
+model %>% save_model_hdf5("cats_and_dogs_small_2.h5")
+plot(history)
